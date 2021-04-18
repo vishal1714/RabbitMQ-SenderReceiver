306462bd7fc819c857505cf24ce921391eb644b4
Vishal Ghadage <ghadagevishal993@gmail.com>
2021-04-18 20:46:50 +0530

    Update MQReceiver.js

--- MQReceiver.js
+++ MQReceiver.js
@@ -1,6 +1,8 @@
 var amqp = require('amqplib/callback_api');
 const EmployeeAPILog = require('./models/APILogSchema');
 const express = require('express');
+const fs = require('fs');
+const moment = require('moment');
 
 var AMQ = async () => {
   amqp.connect('amqp://vishal:vishal1714@raje.tech', function (error0, conn) {
@@ -12,10 +14,13 @@ var AMQ = async () => {
         throw error1;
       }
       const Queue = 'APILogDB';
+      const Queue1 = 'APILogFile';
       channel.assertQueue(Queue, {
         durable: true,
       });
-      var i = 0;
+      channel.assertQueue(Queue1, {
+        durable: true,
+      });
       channel.consume(
         Queue,
         (msg) => {
@@ -24,7 +29,33 @@ var AMQ = async () => {
           console.log(
             `Queue Name -> ${Queue} | Published| API ID -> ${Message.MQ_ID} | Meesage Logged Date -> ${Message.Data.LoggedAt}`
           );
-          i++;
+
+        },
+        {
+          noAck: true,
+  }
+      );
+
+      channel.consume(
+        Queue1,
+        (msg) => {
+        const Message = JSON.parse(msg.content.toString());
+        var LogDate = moment()
+        .tz('Asia/Kolkata')
+        .format('MMMM Do YYYY, hh:mm:ss A');
+      var FileDate = moment().tz('Asia/Kolkata').format('YYYY-MM-DD');
+        var LogedinDB = JSON.stringify(Message.Data);
+        //console.log('Log' + LogedinDB);
+        var LogData = '|' + LogDate + '|' + LogedinDB;
+
+        let filename = './Logs/APILog' + '-' + FileDate + '.log';
+        // var logStream = fs.createWriteStream(filename, { flags: 'a' });
+        // use {flags: 'a'} to append and {flags: 'w'} to erase and write a new file
+        // logStream.write(LogData + '\n');
+
+        fs.appendFile(filename, LogData + '\n', function (err) {
+          if (err) throw err;
+        });
         },
         {
           noAck: true,
@@ -34,4 +65,11 @@ var AMQ = async () => {
   });
 };
 
-module.exports = AMQ;
+function CreatePath(filePath) {
+    if (fs.existsSync(filePath)) {
+    } else {
+      fs.mkdirSync(filePath);
+    }
+  }
+
+  module.exports = { AMQ, CreatePath };
